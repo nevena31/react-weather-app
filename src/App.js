a5ea06d808f65f0118c88dfb9200e371c6ed474c
nevena31 <125161542+nevena31@users.noreply.github.com>
2023-07-30 01:19:00 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Nevena Ivaštanin and is{" "}
-        <a
-          href="https://github.com/nevena31/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Nevena Ivaštanin and is{" "}
+          <a
+            href="https://github.com/nevena31/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
