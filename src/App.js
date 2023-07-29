27eb91ae9c5a7d995c156c127be172efa462f126
nevena31 <125161542+nevena31@users.noreply.github.com>
2023-07-30 00:59:53 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Nevena Ivaštanin and is{" "}
+        <a
+          href="https://github.com/nevena31/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
