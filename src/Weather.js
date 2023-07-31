b28081dc1f3f59e6cc44f04884035f915efb2d6b
nevena31 <125161542+nevena31@users.noreply.github.com>
2023-07-31 17:56:17 +0200

    Added forecast API

--- src/Weather.js
+++ src/Weather.js
@@ -59,7 +59,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast city={weatherData.city} />
       </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "aa8be038534a124at608fo4af5b10b9a";
+  let city = props.city;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
